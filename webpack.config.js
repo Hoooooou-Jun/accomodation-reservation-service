063addc87507a79a6630d57b6447a5c8de1f3e50
Hoooooou-Jun <jipkim2@gmail.com>
2023-03-27 23:58:19 +0900

    Initial: webpack 세팅

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,18 @@
+const path = require('path');
+
 module.exports = {
-  // ...
+  mode: 'development',
+  entry: path.join(__dirname, 'src/pages', '_app.tsx'),
+  output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'index.js',
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx"],
+  },
   module: {
     rules: [
+      { test: /\.tsx$/, use: ['babel-loader', 'ts-loader'] },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
         use: [
@@ -13,13 +24,8 @@ module.exports = {
           }
         ]
       },
-      {
-        test: /\.scss$/,
-        use: [
-          'style-loader',
-          'css-loader',
-        ],
-      },
+      { test: /\.scss$/, use: ['style-loader', 'css-loader'] },
     ],
   },
+  optimization: { minimizer: [] },
 };
